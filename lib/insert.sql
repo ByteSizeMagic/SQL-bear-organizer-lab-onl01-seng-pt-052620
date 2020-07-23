8d8370014c410459207784b77b319141c01aeee5
ByteSizeMagic <bytesizemagic@gmail.com>
2020-07-22 21:40:18 -0700

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Mr. Chocolate', null, 'M', 'brown', 'docile', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Rowdy', 3, 'F', 'black', 'playful', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Tabitha', 6, 'F', 'brown', 'dominant', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Sargeant Brown', 7, 'M', 'brown', 'dominant', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Melissa', 2, 'F', 'black', 'goofy', 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Grinch', 4, 'M', 'black', 'docile', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Wendy', 4, 'F', 'brown', 'docile', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (null, 7, 'M', 'brown', 'agressive', 0);
